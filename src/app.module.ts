09d607f645e7022b7c1c8c84bb8fcf04f7cdabd3
v.mahdych <mahdych.vwork@gmail.com>
2024-06-25 21:32:07 +0300

    core - Init project infrastructure.
    - init project
    - setup base packages for DB
    - configure ConfigurationModule
    - init TypeOrm Connection
    - generate empty Player Module

---
+++ src/app.module.ts
@@ -0,0 +1,40 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TelegrafModule } from 'nestjs-telegraf';
+import { session } from 'telegraf';
+
+import { PlayerModule } from './player/player.module';
+import { configValidationSchema } from './config.schema';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      validationSchema: configValidationSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        autoLoadEntities: true,
+        synchronize: true,
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+      }),
+    }),
+    TelegrafModule.forRootAsync({
+      botName: 'wow_raid_helper_bot',
+      useFactory: () => ({
+        token: process.env.BOT_TOKEN,
+        middlewares: [session()],
+      }),
+    }),
+    PlayerModule,
+  ],
+})
+export class AppModule {}
